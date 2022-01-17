613fecc919af87b51cf09ee37dac55627c736b4a
Rubel Hossain <rubel8525@yahoo.com>
2022-01-18 01:43:13 +0600

    React scope added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../assets/css/App.css";
 import TodoWrapper from "./TodoWrapper"
--- src/Components/TodoWrapper/index.jsx
+++ src/Components/TodoWrapper/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import SingleTodoItem from "../SingleTodoItem"
 
